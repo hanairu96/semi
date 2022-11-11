4f92cc2edccca906b90bf3f197070ab7a6efb059
dllo0 <dllo0@DESKTOP-HGKV66H>
2022-11-11 16:44:12 +0900

    04_bs main 더하기 연산, 설정파일 추가

--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -2,6 +2,9 @@ package com.git.run;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		System.out.println("Hello Git!!");
+		int a=10;
+		int b=10;
+		System.out.println(a+b);
 	}
 }
