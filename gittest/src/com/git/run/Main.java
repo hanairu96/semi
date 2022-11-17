01a1ea70b36dcb34445f3454df31d6773715cc81
dllo0 <dllo0@DESKTOP-HGKV66H>
2022-11-17 09:14:32 +0900

    01_bs 정수 변수 추가, 더하기 출력

--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -1,8 +1,12 @@
 package com.git.run;
 
 public class Main {
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-
+		
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 	}
 }
